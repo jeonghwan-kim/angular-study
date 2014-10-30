ba9e54f25bd416425e341c6422830f8613973465
jeonghwan <ej88ej@gmail.com>
2014-10-30 11:23:22 +0900

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularStudyApp
+ * @description
+ * # angularStudyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularStudyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
